0a080dba6561261da295862d9e3e96a4e0cf544c
AnimewkaSes <brol0526@gmail.com>
2023-01-11 15:07:55 +0600

    Созданны базовые переменные

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
-﻿internal class Program
+﻿using System.Text.RegularExpressions;
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Программа по форматированию массива строк в новый");
+        string r = ("1234567890!#h:-)(+$%^&*=+");
+        string[] arrString = {"Hello", "2", "world", ":-)"};
+        Console.Write("Текущий массив: [");
+        for (int i = 0; i < arrString.Length; i++)
+        {
+            Console.Write(" {0} ", arrString[i]);
+        }
+        Console.Write("]");
+        
+        
+        if (arrString.Contains(r))
+        {
+            Console.WriteLine(string.Format("{0}"),r);
+        }
     }
 }
